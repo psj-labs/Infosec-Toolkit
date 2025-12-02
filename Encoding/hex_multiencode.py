eb941c5c67b32d4df86db15a11f1b96b0ab636bd
SUNGJIN PARK <sungjin1014@proton.me>
2025-12-02 21:06:42 +0900

    Create hex_multiencode.py

---
+++ Encoding/hex_multiencode.py
@@ -0,0 +1,13 @@
+def hex_encode_message(text, count):
+    encoded_text = text
+
+    while count > 0:
+        encoded_text = encoded_text.encode("utf-8").hex()
+        count -= 1
+
+    return encoded_text
+
+
+message = "admin"
+
+print(hex_encode_message(message, 10))   # 10 times HEX encoding
